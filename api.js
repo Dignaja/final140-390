6cc666b7c71884071df8fba84d5ee80bc57e8682
Dignaja <Digjirapat@gmail.com>
2019-12-03 18:36:28 +0700

    #7 finish

--- api.js
+++ api.js
@@ -30,10 +30,12 @@ function updateProductById(req, res) {
     var payload = req.body
     var pid = req.params.pid;    
     // #7 Update a product by ID (findByIdAndUpdate)
-
+    Product.findByIdAndUpdate(payload, pid, function(err){
+        if (err) res.status(500).json(err);
+        res.json({status: "Update a product"});   
+        })
     // ===============================
 }
-
 function deleteProductById(req, res) {
     var pid = req.params.pid;    
     // #8 Delete a product by ID (findByIdAndDelete)
